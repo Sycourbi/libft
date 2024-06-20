7354d2ff95af2ce4f0fa95d856ec7637df6e92bc
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:31:07 +0200

    Update ft_isprint.c

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -9,7 +9,13 @@
 /*   Updated: 2022/11/07 13:42:23 by sycourbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include "libft.h"
+/*
+** Vérifie si le caractère donné est imprimable, c'est-à-dire s'il est affichable.
+**
+** \param c Le caractère à tester.
+** \return 1 si c'est un caractère imprimable, 0 sinon.
+*/
 int	ft_isprint(int c)
 {
 	if (c >= 32 && c <= 126)
