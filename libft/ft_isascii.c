92c45d56cf1490211ed96fdd65126828c9a1d780
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:29:18 +0200

    Update ft_isascii.c

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -9,7 +9,13 @@
 /*   Updated: 2022/11/11 12:36:33 by sycourbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include "libft.h"
+/*
+** Vérifie si le caractère donné est un caractère ASCII (0 à 127).
+**
+** \param c Le caractère à tester.
+** \return 1 si c'est un caractère ASCII, 0 sinon.
+*/
 int	ft_isascii(int c)
 {
 	if (c >= 0 && c <= 127)
