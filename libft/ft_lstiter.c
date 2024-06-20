9e0315589b8f6793c958d454b40f10be72ed1fb8
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:45:01 +0200

    Update ft_lstiter.c

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -10,7 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
+/*
+** Itère sur chaque élément de la liste et applique la fonction `f`.
+**
+** \param lst Pointeur vers le premier élément de la liste.
+** \param f La fonction à appliquer sur chaque élément.
+** \return Aucun retour.
+*/
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst)
