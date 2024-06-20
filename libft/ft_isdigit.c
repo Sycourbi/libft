d042d4104ee2af3d21441a9dda47f504e8308058
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:30:10 +0200

    Update ft_isdigit.c

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -9,7 +9,13 @@
 /*   Updated: 2022/11/07 13:42:03 by sycourbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include "libft.h"
+/*
+** Vérifie si le caractère donné est un caractère numerique.
+**
+** \param c Le caractère à tester.
+** \return 1 si c'est un caractère numerique, 0 sinon.
+*/
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
