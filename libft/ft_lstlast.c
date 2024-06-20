b04b410cb7ac962e431e4fae6053d1ed9e49f574
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:43:19 +0200

    Update ft_lstlast.c

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -10,7 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
+/*
+** Renvoie le dernier élément de la liste.
+**
+** \param lst Pointeur vers le premier élément de la liste.
+** \return Un pointeur vers le dernier élément de la liste, ou NULL si la liste est vide.
+*/
 t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst == NULL)
