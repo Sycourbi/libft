6dd99306ef79e2b469a2dd6bba358229b089a979
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:40:01 +0200

    Update ft_toupper.c

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -9,7 +9,13 @@
 /*   Updated: 2022/11/07 13:45:46 by sycourbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include "libft.h"
+/*
+** Convertit le caractère donné en majuscule s'il est alphabétique en minuscule.
+**
+** \param c Le caractère à convertir.
+** \return Le caractère converti en majuscule, ou tel quel si non alphabétique.
+*/
 int	ft_toupper(int c)
 {
 	if (c >= 'a' && c <= 'z')
