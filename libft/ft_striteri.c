9ba4c691cd0f84996b9a01a1d6fddcbb40aedaae
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:51:28 +0200

    Update ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -10,7 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
+/*
+** Applique la fonction `f` à chaque caractère de la chaîne de caractères `s`,
+** en lui passant son index en premier argument.
+**
+** \param s La chaîne de caractères à modifier.
+** \param f La fonction à appliquer à chaque caractère avec son index.
+** \return Aucun retour.
+*/
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	int	i;
