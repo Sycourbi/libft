b2c52e4b39289615d9c71321f542fc0f39cde6c3
Sycourbi <119012287+Sycourbi@users.noreply.github.com>
2024-06-20 16:39:35 +0200

    Update ft_tolower.c

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -9,7 +9,13 @@
 /*   Updated: 2022/11/07 13:45:24 by sycourbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include "libft.h"
+/*
+** Convertit le caractère donné en minuscule s'il est alphabétique en majuscule.
+**
+** \param c Le caractère à convertir.
+** \return Le caractère converti en minuscule, ou tel quel si non alphabétique.
+*/
 int	ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
